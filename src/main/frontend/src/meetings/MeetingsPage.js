abf9811a914689c9065666241baf399110a06c8e
Klaudia Trepa <klaudia.trepa@gmail.com>
2026-05-16 15:52:57 +0200

    added logic to add new meetings

--- src/main/frontend/src/meetings/MeetingsPage.js
+++ src/main/frontend/src/meetings/MeetingsPage.js
@@ -1,33 +1,46 @@
-import {useState} from "react";
+import { useState } from "react";
 import NewMeetingForm from "./NewMeetingForm";
 import MeetingsList from "./MeetingsList";
 
-export default function MeetingsPage({username}) {
-    const [meetings, setMeetings] = useState([]);
-    const [addingNewMeeting, setAddingNewMeeting] = useState(false);
+export default function MeetingsPage({ username }) {
+  const [meetings, setMeetings] = useState([]);
+  const [addingNewMeeting, setAddingNewMeeting] = useState(false);
 
-    function handleNewMeeting(meeting) {
-        const nextMeetings = [...meetings, meeting];
-        setMeetings(nextMeetings);
-        setAddingNewMeeting(false);
+  async function handleNewMeeting(meeting) {
+    const response = await fetch("/api/meetings", {
+      method: "POST",
+      body: JSON.stringify(meeting),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      const nextMeetings = [...meetings, meeting];
+      setMeetings(nextMeetings);
+      setAddingNewMeeting(false);
     }
+  }
 
-    function handleDeleteMeeting(meeting) {
-        const nextMeetings = meetings.filter(m => m !== meeting);
-        setMeetings(nextMeetings);
-    }
+  function handleDeleteMeeting(meeting) {
+    const nextMeetings = meetings.filter((m) => m !== meeting);
+    setMeetings(nextMeetings);
+  }
 
-    return (
-        <div>
-            <h2>Zajęcia ({meetings.length})</h2>
-            {
-                addingNewMeeting
-                    ? <NewMeetingForm onSubmit={(meeting) => handleNewMeeting(meeting)}/>
-                    : <button onClick={() => setAddingNewMeeting(true)}>Dodaj nowe spotkanie</button>
-            }
-            {meetings.length > 0 &&
-                <MeetingsList meetings={meetings} username={username}
-                              onDelete={handleDeleteMeeting}/>}
-        </div>
-    )
+  return (
+    <div>
+      <h2>Zajęcia ({meetings.length})</h2>
+      {addingNewMeeting ? (
+        <NewMeetingForm onSubmit={(meeting) => handleNewMeeting(meeting)} />
+      ) : (
+        <button onClick={() => setAddingNewMeeting(true)}>
+          Dodaj nowe spotkanie
+        </button>
+      )}
+      {meetings.length > 0 && (
+        <MeetingsList
+          meetings={meetings}
+          username={username}
+          onDelete={handleDeleteMeeting}
+        />
+      )}
+    </div>
+  );
 }
